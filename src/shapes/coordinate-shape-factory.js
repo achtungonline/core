85376d674d79b993885a6a0deed3b5f6bcd46f2b
Mathias Lindblom <mathias.lindblom@trustly.com>
2015-06-29 19:40:33 +0200

    Added functions in CoordShape that extends Shape functions

--- src/shapes/coordinate-shape-factory.js
+++ src/shapes/coordinate-shape-factory.js
@@ -1,14 +1,30 @@
 var clone = require("./../util/clone.js");
+var ShapeFactory = require("./shape-factory.js");
 
 module.exports = function CoordinateShapeFactory() {
+    var shapeFactory = ShapeFactory();
+
+    var createCoordShape = function(shape, x, y) {
+        var coordinateShape = clone(shape);
+        coordinateShape.x = x;
+        coordinateShape.y = y;
+        coordinateShape.maxX = x + shape.boundingBox.width;
+        coordinateShape.maxY = y + shape.boundingBox.height;
+        coordinateShape.centerX = x + shape.boundingBox.width / 2;
+        coordinateShape.centerY = y + shape.boundingBox.height / 2;
+        return coordinateShape;
+    }
+    
     return {
-        create: function(x, y, shape) {
-            var coordinateShape = clone(shape);
-            coordinateShape.x = x;
-            coordinateShape.y = y;
-            coordinateShape.maxX = x + shape.boundingBox.width;
-            coordinateShape.maxY = y + shape.boundingBox.height;
-            return coordinateShape;
+        create: createCoordShape,
+        createCircle: function(radius, x, y) {
+            createCoordShape(shapeFactory.createCircle(radius), x, y);
+        },
+        createRectangle: function(width, height, x, y) {
+            createCoordShape(shapeFactory.createRectangle(width, height), x, y);
+        },
+        createSquare: function(size, x, y) {
+            createCoordShape(shapeFactory.createSquare(size), x, y);
         }
     }
 };
