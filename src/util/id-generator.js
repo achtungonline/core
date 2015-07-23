a785f9078bd1887d3f78b94b25ccc52f53275bea
Mathias Lindblom <mathias.lindblom@trustly.com>
2015-07-23 14:28:35 +0200

    Forgot ID-generator

---
+++ src/util/id-generator.js
@@ -0,0 +1,16 @@
+var idGenerator = module.exports = {}
+
+idGenerator.indexCounterId = function (startingIndex) {
+    return function nextIndexFunction() {
+        var id = startingIndex;
+        startingIndex++;
+        return id;
+    }
+}
+
+idGenerator.UUID = function () {
+    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
