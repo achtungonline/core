9e3cacf12e1a6ac627124075c4d148b841d5d48a
Mathias Lindblom <mathias.lindblom@trustly.com>
2015-07-29 09:46:24 +0200

    Map bounding box now gets created in the constructor as with shapes

--- src/map-utils.js
+++ src/map-utils.js
@@ -1,30 +1,11 @@
 var utils = module.exports = {};
 
-utils.getBoundingBox = function (map) {
-    var boundingBox = {
-        width: 0,
-        height: 0
-    };
-
-    map.shapes.forEach(function (shape) {
-        if (shape.x + shape.boundingBox.width > boundingBox.width) {
-            boundingBox.width = shape.x + shape.boundingBox.width;
-        }
-
-        if (shape.y + shape.boundingBox.height > boundingBox.height) {
-            boundingBox.height = shape.y + shape.boundingBox.height;
-        }
-    });
-
-    return boundingBox;
-};
-
 utils.isInsidePlayableArea = function (map, shape) {
     //TODO: This needs to be done better.
 
     var mapX = map.shapes[0].x;
     var mapY = map.shapes[0].y;
-    var mapBoundingBox = utils.getBoundingBox(map);
+    var mapBoundingBox = map.boundingBox;
 
     if (shape.x + shape.boundingBox.width > mapX + mapBoundingBox.width) {
         return false;
--- src/map.js
+++ src/map.js
@@ -1,5 +1,27 @@
 module.exports = function Map(shapes) {
-    return {
-        shapes: shapes
+
+    function createBoundingBox() {
+        var boundingBox = {
+            width: 0,
+            height: 0
+        };
+
+        shapes.forEach(function (shape) {
+            if (shape.x + shape.boundingBox.width > boundingBox.width) {
+                boundingBox.width = shape.x + shape.boundingBox.width;
+            }
+
+            if (shape.y + shape.boundingBox.height > boundingBox.height) {
+                boundingBox.height = shape.y + shape.boundingBox.height;
+            }
+        });
+
+        return boundingBox;
     };
+
+    var map = {};
+
+    map.shapes = shapes;
+    map.boundingBox = createBoundingBox();
+    return map;
 };
