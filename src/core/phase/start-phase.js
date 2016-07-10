da1eeb759823e8b6221b3d84d30500474799a645
Mathias Lindblom <ommatte@gmail.com>
2016-07-10 16:52:16 +0200

    Worms can no longer spawn right next to eachother

--- src/core/phase/start-phase.js
+++ src/core/phase/start-phase.js
@@ -1,3 +1,5 @@
+var shapeModifierI = require("./../geometry/shape-modifier-immutable-factory.js")().create();
+
 var PHASE_DURATION = 2.5; // seconds
 
 var startPhase = module.exports = {};
@@ -14,10 +16,11 @@ startPhase.StartPhase = function StartPhase(deps) {
     var playerHandler = deps.playerHandler;
 
     function setWormStartingPositions(gameState, worms, map) {
-        function isCollidingWithWorms(worms, shape) {
+        function isTooCloseToOtherWorms(worms, shape) {
+            var biggerShape = shapeModifierI.changeSize(shape, 70, 70);
             for (var i in worms) {
                 var worm = worms[i];
-                if (shapeSpatialRelations.intersects(worm.head, shape)) {
+                if (shapeSpatialRelations.intersects(worm.head, biggerShape)) {
                     return true;
                 }
             }
@@ -32,7 +35,7 @@ startPhase.StartPhase = function StartPhase(deps) {
         playerUtils.forEachAliveWorm(worms, function (worm) {
             var newHead = getWormHeadInsidePlayableMapArea(worm);
             var counter = 0;
-            while (isCollidingWithWorms(updatedWorms, newHead)) {
+            while (isTooCloseToOtherWorms(updatedWorms, newHead)) {
                 if (counter > 100000) {
                     throw Error("Failed to find starting position for worm that does not collide with other worms.");
                 }
