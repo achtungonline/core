8a35fffc0a3e35258989e2356dcf58674afc436d
Mathias Lindblom <ommatte@gmail.com>
2016-07-15 11:15:14 +0200

    Updated speed powerup

--- src/core/power-up/effect-definitions/speed.js
+++ src/core/power-up/effect-definitions/speed.js
@@ -1,7 +1,10 @@
 var TYPE = "speed";
 
+var FADE_DURATION = 0.4;
+
 function activate(gameState, strength, duration, wormId) {
     return {
+        duration: duration,
         timeLeft: duration,
         strength: strength,
         wormId: wormId,
@@ -10,6 +13,16 @@ function activate(gameState, strength, duration, wormId) {
 }
 
 function changeSpeed(gameState, effect, wormSpeed) {
+    var timeActive = effect.duration - effect.timeLeft;
+
+    if (timeActive <= FADE_DURATION) {
+        // Being applied.
+        return wormSpeed * (1 + ((effect.strength - 1) * timeActive / FADE_DURATION));
+    } else if (effect.timeLeft <= FADE_DURATION) {
+        // Fading away.
+        return wormSpeed * (1 + ((effect.strength - 1) * effect.timeLeft / FADE_DURATION));
+    }
+
     return wormSpeed * effect.strength;
 }
 
