ba15852bcb0a4ed20c44f047e4b263e5f6a0c232
Mathias Lindblom <ommatte@gmail.com>
2016-07-15 11:15:32 +0200

    Updated drunk powerup

--- src/core/power-up/effect-definitions/drunk.js
+++ src/core/power-up/effect-definitions/drunk.js
@@ -1,6 +1,8 @@
 var random = require("./../../util/random.js");
 var TYPE = "drunk";
 
+var FADE_DURATION = 0.3;
+
 function activate(gameState, strength, duration, wormId) {
     return {
         timeLeft: duration,
@@ -8,17 +10,36 @@ function activate(gameState, strength, duration, wormId) {
         wormId: wormId,
         type: TYPE,
         speedChange: 0,
+        speedChangeTimeLeft: 0,
+        prevSpeedChange: 0,
         turningVelocityChange: 0
     };
 }
 
 function update(gameState, deltaTime, effect) {
-    effect.speedChange += (random.random(gameState) - 0.5) * deltaTime * 800 - (effect.speedChange * deltaTime * 20);
-    effect.turningVelocityChange += (random.random(gameState) -0.5) * deltaTime * 200 - (effect.turningVelocityChange * deltaTime * 20);
+    if (effect.speedChangeTimeLeft <= 0) {
+        effect.prevSpeedChange = effect.speedChange;
+        effect.speedChangeTimeLeft = FADE_DURATION;
+        effect.speedChange = random.randInt(gameState, -25, 25); //(random.random(gameState) - 0.5) * deltaTime * 50 - (effect.speedChange * deltaTime * 20);
+    }
+    effect.speedChangeTimeLeft -= deltaTime;
+
+    effect.turningVelocityChange += (random.random(gameState) - 0.5) * deltaTime * 200 - (effect.turningVelocityChange * deltaTime * 20);
 }
 
 function changeSpeed(gameState, effect, speed) {
-    return speed + effect.strength * effect.speedChange;
+    var speedChangeDiff = effect.speedChange - effect.prevSpeedChange;
+    var speedChange = effect.strength * (effect.prevSpeedChange + speedChangeDiff * (1 - effect.speedChangeTimeLeft / FADE_DURATION));
+
+    var timeActive = effect.duration - effect.timeLeft;
+    if (timeActive <= FADE_DURATION) {
+        // Being applied.
+        return speed + (speedChange * timeActive / FADE_DURATION);
+    } else if (effect.timeLeft <= FADE_DURATION) {
+        // Fading away.
+        return speed + (speedChange * effect.timeLeft / FADE_DURATION);
+    }
+    return speed + speedChange;
 }
 
 function changeTurningVelocity(gameState, effect, turningVelocity) {
