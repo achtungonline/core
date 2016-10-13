fde44e6709a95240a350376006b8f965887ca31d
Erik Odenman <erik.odenman@gmail.com>
2016-10-13 13:58:43 +0200

    Fixed compression after clear changes

--- src/core/util/compression.js
+++ src/core/util/compression.js
@@ -2,51 +2,31 @@ var trajectoryUtil = require("../geometry/trajectory/trajectory-util.js");
 
 // TODO Should replace this with some binary format, probably protobuf
 function compressWormSegment(segment) {
-    if (segment.type === "clear") {
-        return [
-            segment.type,
-            segment.startTime.toFixed(3),
-            segment.endTime.toFixed(3),
-            segment.index
-        ];
-    } else {
-        return [
-            segment.duration.toFixed(3),
-            segment.index,
-            segment.jump,
-            segment.playerId,
-            segment.size,
-            segment.speed,
-            segment.startDirection.toFixed(3),
-            segment.startTime.toFixed(3),
-            segment.startX.toFixed(3),
-            segment.startY.toFixed(3),
-            segment.turningVelocity
-        ];
-    }
+    return [
+        segment.type,
+        segment.duration.toFixed(3),
+        segment.index,
+        segment.jump,
+        segment.playerId,
+        segment.size,
+        segment.speed,
+        segment.startDirection.toFixed(3),
+        segment.startTime.toFixed(3),
+        segment.startX.toFixed(3),
+        segment.startY.toFixed(3),
+        segment.turningVelocity
+    ];
 }
 
 function decompressWormSegment(data) {
-    if (data.length === 4) {
-        return {
-            type: data[0],
-            startTime: parseFloat(data[1]),
-            endTime: parseFloat(data[2]),
-            index: data[3]
-        }
-    }
-    var duration = parseFloat(data[0]);
-    var index = data[1];
-    var jump = data[2];
-    var playerId = data[3];
-    var size = data[4];
-    var speed = data[5];
-    var startDirection = parseFloat(data[6]);
-    var startTime = parseFloat(data[7]);
-    var startX = parseFloat(data[8]);
-    var startY = parseFloat(data[9]);
-    var turningVelocity = data[10];
+    var [type, duration, index, jump, playerId, size, speed, startDirection, startTime, startX, startY, turningVelocity] = data;
+    duration = parseFloat(duration);
+    startDirection = parseFloat(startDirection);
+    startTime = parseFloat(startTime);
+    startX = parseFloat(startX);
+    startY = parseFloat(startY);
     var trajectory = trajectoryUtil.createTrajectory({startX, startY, startDirection, speed, turningVelocity, duration});
+    trajectory.type = type;
     trajectory.index = index;
     trajectory.jump = jump;
     trajectory.playerId = playerId;
